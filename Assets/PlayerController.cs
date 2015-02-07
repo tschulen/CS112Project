fbdacea0ff694af19c7de3fa441b6f0f02c81e38
BJarv <brandon.jarv@gmail.com>
2015-02-06 19:09:15 -0800

    Base

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	float moveH;
+	public bool colliding = false;
+	//public float moveSpeed = 
+	//private GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		moveH = Input.GetAxis("Horizontal");
+
+		if(moveH < 0) {
+			rigidbody2D.AddForce (new Vector2 (-50, 0));
+		} else if (moveH > 0) {
+			rigidbody2D.AddForce (new Vector2 (50,0));
+		} else {
+		
+		}
+	}
+}
