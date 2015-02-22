c02f9d40fff8b289b1c13cdf5ad8e020ab6bef12
gcherr <gcherrer@ucsc.edu>
2015-02-22 01:36:45 -0800

    Dash, Health, Health Bars implemented
    Movement, Win Conditions unfinished

---
+++ Assets/Scripts/Standard Assets/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = startingHealth; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthSlider.value = currentHealth;
+		if (currentHealth == 0)
+			isDead = true;
+	}
+}
